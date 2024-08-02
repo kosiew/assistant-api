d5423734c15b8f683c05865725dee6b4735ffa23
Siew Kam Onn <kosiew@gmail.com>
2024-08-02 10:27:04 +0800

    iniital commit

---
+++ activate_openai_1.14.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+# Script to activate Python virtual environment
+VENV_PATH="$VENV_ROOT/openai_1.14" activate_venv
+
---
+++ activate_venv.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+# Script to activate Python virtual environment
+VENV_PATH="$VENV_ROOT/openai_1.30" activate_venv
+
